37a7bb73cbce71229ab2bf5ed75deffa094124ad
diego <diegojavier.ramirezzh@gmail.com>
2023-05-22 19:26:29 +0200

    El segundo test probara los multiplos de 5, se espera que devuelva 'Miga'

--- TestGachamiga.java
+++ TestGachamiga.java
@@ -2,11 +2,18 @@ import practica.v2.Gachamiguero2;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class TestGachamiga {
+public class  TestGachamiga {
     @Test
     public void TestMultiplo3(){
         Gachamiguero2 test = new Gachamiguero2();
         String result = test.obtenerGachamiga(6);
         Assertions.assertEquals("Gacha", result);
+    }
+
+    @Test
+    public void TestMultiplo5(){
+        Gachamiguero2 test = new Gachamiguero2();
+        String result = test.obtenerGachamiga(5);
+        Assertions.assertEquals("Miga", result);
     }
 }
