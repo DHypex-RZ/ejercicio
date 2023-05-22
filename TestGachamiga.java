48ed30b078cb2d538c5b69d1661cee03e17d746d
diego <diegojavier.ramirezzh@gmail.com>
2023-05-22 19:02:54 +0200

    segundo test que se realiza a Gachamiguero

--- TestGachamiga.java
+++ TestGachamiga.java
@@ -9,4 +9,11 @@ public class TestGachamiga1 {
         String result = test.obtenerGachamiga(1);
         Assertions.assertEquals("1", result);
     }
+
+    @Test
+    public void testFor2(){
+        Gachamiguero2 test = new Gachamiguero2();
+        String result = test.obtenerGachamiga(2);
+        Assertions.assertEquals("2", result);
+    }
 }
