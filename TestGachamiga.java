7a9f08888f9c2772d91ff7c3cbbf713854d77d3c
diego <diegojavier.ramirezzh@gmail.com>
2023-05-22 19:33:55 +0200

    Nueva version de los test, primer test que se realiza

---
+++ TestGachamiga.java
@@ -0,0 +1,7 @@
+public class TestGachamiga {
+    @Test
+    public void TestMultiplo3(){
+        String result = GachamigueroV2.cocinarGachamiga(6);
+        Assertions.assertEquals("Gacha", result);
+    }
+}
