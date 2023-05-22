b87687f46b1a238ab8ea8ccd49e53d0ddef7067e
diego <diegojavier.ramirezzh@gmail.com>
2023-05-22 19:34:40 +0200

    segundo test que se realiza

--- TestGachamiga.java
+++ TestGachamiga.java
@@ -1,7 +1,17 @@
+import practica.v2.Gachamiguero2;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TestGachamiga {
     @Test
     public void TestMultiplo3(){
         String result = GachamigueroV2.cocinarGachamiga(6);
         Assertions.assertEquals("Gacha", result);
+    }
+
+    @Test
+    public void TestMultiplo5(){
+        String result = GachamigueroV2.cocinarGachamiga(5);
+        Assertions.assertEquals("Miga", result);
     }
 }
