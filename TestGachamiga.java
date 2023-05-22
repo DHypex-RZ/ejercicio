3e68290ae598b5556b80a4d8f54d4e38f3b563af
diego <diegojavier.ramirezzh@gmail.com>
2023-05-22 19:25:32 +0200

    Se crea un nuevo test, los test que se realizan seran diferentes, en este caso, se haran test para cualquier multiplo de 3, se espera que el test devuelva 'Gacha'

---
+++ TestGachamiga.java
@@ -0,0 +1,12 @@
+import practica.v2.Gachamiguero2;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestGachamiga {
+    @Test
+    public void TestMultiplo3(){
+        Gachamiguero2 test = new Gachamiguero2();
+        String result = test.obtenerGachamiga(6);
+        Assertions.assertEquals("Gacha", result);
+    }
+}
