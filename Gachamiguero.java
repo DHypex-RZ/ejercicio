49f12522a7848b5ef631b3413f2574a2b97caae1
diego <diegojavier.ramirezzh@gmail.com>
2023-05-22 19:21:27 +0200

    Correción de la clase Gachamiguero

--- Gachamiguero.java
+++ Gachamiguero.java
@@ -1,18 +1,8 @@
 public class Gachamiguero {
-    public static void main(String[] args) {
-        Gachamiguero program = new Gachamiguero();
-        program.start();
-    }
-
-    public void start() {
-        for(int i = 1; i <= 100; i++){
-            if(i % 3 == 0 && i % 5 == 0)
-                System.out.println("Gachamiga\t");
-            else if(i%3==0)
-                System.out.print("Gacha\t");
-            else if(i%5==0)
-                System.out.print("Miga\t");
-            else System.out.print(i + "\t");
-        }
+    public static String cocinarGachamiga(int n){
+        if(n%15==0) return "Gachamiga";
+        if(n%3==0) return "Gacha";
+        if(n%5==0) return "Miga";
+        return n+"";
     }
 }
